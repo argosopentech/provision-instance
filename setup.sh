649c8c82826882b023be36f4518592d2a19d8c51
P.J. Finlay <pjappdevelopment@gmail.com>
2021-07-05 14:42:44 -0500

    Added swap space

--- setup.sh
+++ setup.sh
@@ -8,6 +8,15 @@ passwd -d argosopentech
 chsh -s /bin/bash argosopentech
 usermod -aG sudo argosopentech
 
+# Setup argosopentech
 su argosopentech
 curl https://raw.githubusercontent.com/PJ-Finlay/profile/main/profile.sh > /home/argosopentech/.profile.sh
 curl https://raw.githubusercontent.com/PJ-Finlay/profile/main/setup.sh | sh
+source /home/argosopentech/.profile.sh
+
+# Add swap space
+sudo fallocate -l 10G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo swapon --show
